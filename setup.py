54d77bbcc004f71ee12ecca02d68dc2dcda457aa
scopdrag <scopdrag@gmail.com>
2017-05-16 14:53:37 +0530

    pip build uploaded on pypytest

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 
 setup(
     name = 'event_publisher',
-    packages = ['event_publisher'],
+    packages = ['package'],
     version = '0.0.0',
     description = 'Event driven programming using python, attach event handlers and trigger using publisher',
     author = 'Scopdrag',
     author_email = 'scopdrag@gmail.com',
-    url = '',
-    download_url = '',
-    keywords = ['event', 'trigger', 'publisher', 'event-handler'],
-    classifiers = [],
+    url = 'https://github.com/scopdrag/event_publisher',
+    download_url = 'https://github.com/scopdrag/event_publisher/archive/master.zip',
+    keywords = ['event', 'trigger', 'publisher', 'event-handler'], 
 )
